55011b6a4596b9229adafc4bb4f4bc3ec0767ecb
FIRPO LAUTARO <131486673+lautarotecnico@users.noreply.github.com>
2023-05-05 13:43:45 -0300

    Add files via upload

---
+++ tp6 del 2023/tp6 del 2023.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int DDD,Destino,Brasilia,Salvador,SaoPaulo,RioJaneiro,JuizdeFora,Campinas,Victoria,BeloHorizonte;
+	cout<<"DDD:";
+	cout<<61<<endl;
+	cin>>Brasilia;
+	cout<<"el destino es:"<<Brasilia;
+	cout<<71<<endl;
+	cin>>Salvador;
+	cout<<"el destino es:"<<Salvador;
+	cout<<11<<endl;
+	cin>>SaoPaulo;
+	cout<<"el destino es:"<<SaoPaulo;
+	cout<<21<<endl;
+	cin>>RioJaneiro;
+	cout<<"el destino es:"<<RioJaneiro;
+	cout<<32<<endl;
+	cin>>JuizdeFora;
+	cout<<"el destino es:"<<JuizdeFora;
+	cout<<19<<endl;
+	cin>>Campinas;
+	cout<<"el destino es:"<<Campinas;
+	cout<<27<<endl;
+	cin>>Victoria;
+	cout<<"el destino es:"<<Victoria;
+	cout<<31<<endl;
+	cin>>BeloHorizonte;
+	cout<<"el destino es:"<<BeloHorizonte;
+	
+	return 0;
+
+}
